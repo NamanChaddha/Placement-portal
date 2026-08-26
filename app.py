1799910fbcd8df5a85bf6ba64b7810ee5f7adb5d
NamanChaddha <namanchaddha2501@gmail.com>
2026-08-26 23:57:42 +0530

    Fix import formatting
    changed redis functionality

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session,fl
 from flask_mail import Mail, Message
 from celery import Celery
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt 
 
 app= Flask(__name__)
 app.secret_key = 'super_secret_placement_key'
